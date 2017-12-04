430bbd130b17fdd1711c9c590134a1c86be00873
Yoooran <33804037+Yoooran@users.noreply.github.com>
2017-12-04 21:06:25 +0800

    Update GroupControllerTest.java

--- src/test/java/xmu/crms/view/GroupControllerTest.java
+++ src/test/java/xmu/crms/view/GroupControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 /**
  * @author YangYouran
- * @date 2017-12-3
+ * @date 2017-12-4
  */
 
 @RunWith(SpringRunner.class)
@@ -24,6 +24,13 @@ public class GroupControllerTest {
 	@Autowired
     private MockMvc mvc;
 	
+	/**
+     * 按小组ID获取小组详情
+     * url: /group/{groupId}
+     * httpMethod: GET
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testGetGroup() throws Exception {
 		mvc.perform(get("/group/{groupId}",1))
@@ -35,6 +42,13 @@ public class GroupControllerTest {
         	.andDo(print());
 	}
 
+	/**
+     * 组长辞职
+     * url: /group/{groupId}/resign
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testResign() throws Exception {
 		mvc.perform(put("/group/{groupId}/resign",1)
@@ -45,6 +59,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 成为组长
+     * url: /group/{groupId}/assign
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testAssign() throws Exception {
 		mvc.perform(put("/group/{groupId}/assign",1)
@@ -55,6 +76,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 添加成员
+     * url: /group/{groupId}/add
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testAddMember() throws Exception {
 		mvc.perform(put("/group/{groupId}/add",1)
@@ -65,6 +93,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 移除成员
+     * url: /group/{groupId}/remove
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testRemoveMember() throws Exception {
 		mvc.perform(put("/group/{groupId}/remove",1)
@@ -75,6 +110,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 小组按ID选择话题
+     * url: /group/{groupId}/topic
+     * httpMethod: POST
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testChooseTopic() throws Exception {
 		mvc.perform(post("/group/{groupId}/topic",1)
@@ -85,6 +127,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 小组按ID取消选择话题
+     * url: /group/{groupId}/topic/{topicId}
+     * httpMethod: DELETE
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testDeleteTopic() throws Exception {
 		mvc.perform(delete("/group/{groupId}/topic/{topicId}",1,2))
@@ -92,6 +141,13 @@ public class GroupControllerTest {
 	        .andDo(print());
 	}
 	
+	/**
+     * 按ID获取小组成绩
+     * url: /group/{groupId}/grade
+     * httpMethod: GET
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testGetGrade() throws Exception {
 		mvc.perform(get("/group/{groupId}/grade",1))
@@ -104,6 +160,13 @@ public class GroupControllerTest {
 	    	.andDo(print());
 	}
 	
+	/**
+     * 按ID设置小组的报告分
+     * url: /group/{groupId}/grade/report
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testSetReportGrade() throws Exception {
 		mvc.perform(put("/group/{groupId}/grade/report",1)
@@ -114,6 +177,13 @@ public class GroupControllerTest {
 			.andDo(print());
 	}
 	
+	/**
+     * 提交对其他小组的打分
+     * url: /group/{groupId}/grade/presentation/{studentId}
+     * httpMethod: PUT
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testSetPresentationGrade() throws Exception {
 		mvc.perform(put("/group/{groupId}/grade/presentation/{studentId}",1,1)
