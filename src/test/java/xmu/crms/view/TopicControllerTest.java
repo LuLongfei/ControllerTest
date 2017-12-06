d24945c7703ce4fec3e1adc24ef5cd584e4bcbbc
moonlight <1241621063@qq.com>
2017-12-06 16:36:14 +0800

    Update TopicController
    删除多余import

--- src/test/java/xmu/crms/view/TopicControllerTest.java
+++ src/test/java/xmu/crms/view/TopicControllerTest.java
@@ -14,9 +14,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import xmu.crms.controller.TopicController;
-
 /**
  * URL-pattern:prefix="/topic"
  * @author Huhui
